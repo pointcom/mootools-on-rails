d1d34a0495063a7861aab252f4187b0d4ad6cc1f
Grzegorz Kazulak <grzegorzkazulak@GK-2.local>
2008-07-22 08:58:44 +0100

    modified assets helper as well as removed old version of mootools.js

--- lib/rails/asset_tag_helper.rb
+++ lib/rails/asset_tag_helper.rb
@@ -1,16 +1,16 @@
 module ActionView::Helpers::AssetTagHelper
   alias_method :rails_javascript_include_tag, :javascript_include_tag
   
-  JAVASCRIPT_DEFAULT_SOURCES = ['mootools', 'mootools_patch']
+  JAVASCRIPT_DEFAULT_SOURCES = ['mootools-1.2-core', 'mootools-1.2-more', 'application', 'mootools_patch']
   @@javascript_default_sources = JAVASCRIPT_DEFAULT_SOURCES.dup
     
   def javascript_include_tag(*sources)
    if sources.delete :mootools
      sources = sources.concat(
-       ['mootools', behaviours_url]
+       ['mootools-1.2-core', 'mootools-1.2-more', 'application', behaviours_url]
      ).uniq
    elsif sources.delete :defaults
-     sources = ['mootools', 'mootools_patch', behaviours_url].concat(sources)
+     sources = ['mootools-1.2-core', 'mootools-1.2-more', 'mootools_patch', 'application', behaviours_url].concat(sources)
    end
 
    
